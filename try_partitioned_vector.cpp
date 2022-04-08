131593a71f7f21e389b35b2709eafe669936652e
Wenke-D <matthieu.du@outlook.com>
2022-04-08 15:04:44 +0200

    add comments and fix typo

--- try_partitioned_vector.cpp
+++ try_partitioned_vector.cpp
@@ -1,8 +1,7 @@
-#include <string>
-#include <hpx/iostream.hpp>
 #include <hpx/include/partitioned_vector.hpp>
+#include <hpx/iostream.hpp>
 #include <hpx/runtime_distributed/find_localities.hpp>
-
+#include <string>
 
 using string = std::string;
 
@@ -10,15 +9,31 @@ using string = std::string;
 HPX_REGISTER_PARTITIONED_VECTOR(string);
 HPX_REGISTER_PARTITIONED_VECTOR(int);
 
-namespace try_partitioned_vector{
+namespace try_partitioned_vector {
 
-    template <typename T, typename U, typename Multiplier, typename Result,
+/**
+ * @brief Perform the inner product between 2 partitioned vectors. The 2 vectors
+ * are required to have the same size.
+ *
+ * @tparam T element type of the first vector
+ * @tparam U element type of the second vector
+ * @tparam Multiplier the multiplication operation between T and U
+ * @tparam Result the type of final result, aka return type of Accumulater
+ * @tparam Accumulator the accumulation operation between 2 Result
+ * @param a the first vector
+ * @param b the second vector
+ * @param op a multiplication operation
+ * @param accum a accumulation operation
+ * @param init the init value of accumulation
+ * @return Result the result of inner product
+ * @throw std::invalid_argument If 2 incoming vectors don't the same size
+ */
+template <typename T, typename U, typename Multiplier, typename Result,
           typename Accumulater>
-    Result inner_product(
-                    hpx::partitioned_vector<T> &a,
-                    hpx::partitioned_vector<U> &b,
-                    Multiplier op,
-                    Accumulater accum, Result init) {
+Result inner_product(hpx::partitioned_vector<T> &a,
+                     hpx::partitioned_vector<U> &b, Multiplier op,
+                     Accumulater accum,
+                     Result init) throw(std::invalid_argument) {
 
     // 2 operand of inner product must have the same size
     if (a.size() != b.size()) {
@@ -33,11 +48,9 @@ namespace try_partitioned_vector{
     return init;
 }
 
+void test();
 
-
-    void test();
-
-    string str_multiplication(string s, size_t n) {
+string str_multiplication(string s, size_t n) {
     if (n == 0) {
         return "";
     }
@@ -48,8 +61,7 @@ namespace try_partitioned_vector{
 
     return s;
 }
-}
-
+} // namespace try_partitioned_vector
 
 void try_partitioned_vector::test() {
     // instanciate with 10 hello
@@ -57,9 +69,10 @@ void try_partitioned_vector::test() {
     // init with 10 x 2
     hpx::partitioned_vector<int> v3(10, 2);
 
-    auto accum = [](string s1, string s2){return s1 + s2;};
+    auto accum = [](string s1, string s2) { return s1 + s2; };
 
-    auto res = try_partitioned_vector::inner_product(v2, v3, try_partitioned_vector::str_multiplication, accum, string{""});
+    auto res = try_partitioned_vector::inner_product(
+        v2, v3, try_partitioned_vector::str_multiplication, accum, string{""});
 
     hpx::cout << res << hpx::endl;
 }
